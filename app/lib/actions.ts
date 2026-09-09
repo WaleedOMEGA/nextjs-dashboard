24c7373ece27ac7562c7fd1a1c2644121f7ed5fb
Waleed salah <waleed.salah@systemsltd.com>
2026-09-09 10:09:40 +0300

    fix: remove error return messages in create and update invoice functions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,9 +31,7 @@ export async function createInvoice(formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return {
-      message: 'Database Error: Failed to Create Invoice.',
-    };
+    
   }
    revalidatePath('/dashboard/invoices');
    redirect('/dashboard/invoices');
@@ -55,7 +53,7 @@ export async function updateInvoice(id: string, formData: FormData) {
   } catch (error) {
     // We'll also log the error to the console for now
     console.error(error);
-    return { message: 'Database Error: Failed to Update Invoice.' };
+   
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
